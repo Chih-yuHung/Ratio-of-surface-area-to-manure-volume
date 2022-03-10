a24602de4c05383fee61fd5ea2ba62d2b8822046
Koc <efekemal.koc@agr.gc.ca>
2022-03-10 16:42:49 -0500

    My first code

--- ratio_linear regression.R
+++ ratio_linear regression.R
@@ -1,5 +1,25 @@
 #This is to fit a linear regression between the ratio and the difference. 
 
-#This is added by CY
-plot(1:10,2:11)
+
+#My first code March 10th
+
+data<-read.csv("Manure Linear Regression.csv")
+
+
+
+
+plot(data$SV, data$dif)
+
+model.SV <- lm(dif~SV,data=data)
+
+deneme<-data.frame(SV=seq(-17,17,length=50))
+
+#plot simple linear regression model
+
+plot(data$SV, data$dif)
+
+lines(deneme$SV,predict(model.SV,newdata=deneme))
+
+
+
 
