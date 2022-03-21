e2efc932c427ef6698e634035b4f19d476b15f4a
Koc <efekemal.koc@agr.gc.ca>
2022-03-21 10:42:07 -0400

    Swedendata Depth mean

--- Sweden data/swedendatatemp.R
+++ Sweden data/swedendatatemp.R
@@ -12,4 +12,22 @@ head(swedendata2)
 AirT.mean<-tapply(swedendata2$'Ave AirTmax 1 and AirTmin1', swedendata2$Month, mean) 
 list(AirT.mean)
 
-# June: 16.293333 , July: 14.783871  , August: 16.338710                    
+# June: 16.293333 , July: 14.783871  , August: 16.338710  
+
+#Manure Depth Average for June, July, August
+
+head(swedendata)
+depth.mean<-tapply(swedendata$Depth,swedendata$Month,mean)
+list(depth.mean)
+
+# June = 0.6114575 , July = 0.8159436 , August = 1.1245895
+
+
+
+
+
+
+
+
+
+
