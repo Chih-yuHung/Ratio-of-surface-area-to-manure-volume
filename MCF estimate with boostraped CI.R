c3db1c259618bc59c40b69c2a362caba5617131d
Hung <chih-yu.hung@agr.gc.ca>
2022-03-21 22:11:52 -0400

    introduce the MCF calculator

--- MCF estimate with boostraped CI.R
+++ MCF estimate with boostraped CI.R
@@ -29,5 +29,40 @@ temp.dif<-c(temp.predict[1,2],temp.predict[2,3])#it's the lowest dif and highest
 
 
 #Now we can use the MCF calculator to see the change of MCF
+T.avg.m<-read.table("monthly avg T.txt",header=TRUE) # monthly average
 
+#This is an R procedure example how to calculate MCF for different locations. 
+library(weathermetrics) #convert C to K
+
+#Required input, which don't influence MCF
+VS_Yr<-1200  #kg/yr
+VS_LQD<-100  #100%
+#constants, these inputs are fixed input in the 2019 IPCC Refinement
+f_T1<-308.16 #K
+f_Ea<-19347  #cal/mol
+f_R<-1.987   #cal/K.mol
+B0<-0.24     #dimensionless
+#Optional inputs, which influence MCF
+f_Tmin<-1.0  #degree C
+f_T2d<-3.0   #T2 damping, degree C
+E_eff<-95    #95%
+
+M.rm<-ifelse(T.avg.m$Removal.two=="Y",1,0)#convert it for calculation
+#Calculate the MCF for the input locations
+#We're runing two scenarios, low (-3.398) and high (+0.720) 
+#under the two removal scenario, early spring-early fall
+
+#MCF.low is the low temperature difference
+MCF.low<-c()
+for (k in 1:3){
+  M.rm<-ifelse(T.avg.m[,3+k]=="Y",1,0)#convert it for calculation
+  Manure.rm<-rep(M.rm,3) # for stabilization 3 yr
+  T.sel<-T.avg.m[,3] #Use Atlantic Canada for example
+  print(paste("Station sequence",k))
+  source("MCF calculator_single.R",echo = F)
+  MCF.2[k]<-MCF
+}
+print(MCF.2)#0.35 for one removal, 0.24 for two removal, 0.18 for three removal
+
+#MCF.high is the high temperature difference
 
