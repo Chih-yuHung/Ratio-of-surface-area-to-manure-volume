d057f4fa9f71b0ba40974f14c78f574c0dfc8067
Hung <chih-yu.hung@agr.gc.ca>
2022-03-24 15:12:46 -0400

    CI estimate change

--- MCF estimate with boostraped CI.R
+++ MCF estimate with boostraped CI.R
@@ -8,7 +8,7 @@ ratio<-ratio[-6,] #Look likes an outlier
 #do the bootstrap
 set.seed(2022)
 SV.n<-30 #number of samples we want to boostrap everytime
-B<-10000 #bootstrap sample size
+B<-100 #bootstrap sample size
 #Obtain the boostrap samples
 Boot.SV<-matrix(sample(ratio$SV,size=B*SV.n,
                 replace=TRUE),ncol=B,nrow=SV.n)
@@ -16,16 +16,16 @@ Boot.SV<-matrix(sample(ratio$SV,size=B*SV.n,
 Boot.SV.mean<-colMeans(Boot.SV)
  
 #Obtain the percentile
-Boot.low<-quantile(Boot.SV.mean,prob=0.025,names=FALSE) #0.3903
-Boot.up<-quantile(Boot.SV.mean,prob=0.975,names=FALSE) #0.4173
+#Boot.low<-quantile(Boot.SV.mean,prob=0.025,names=FALSE) #0.3903
+#Boot.up<-quantile(Boot.SV.mean,prob=0.975,names=FALSE) #0.4173
 
 #We're going to apply this to the linear regression 
 #and obtain 95% CI for the temperature difference
 temp.lm<-lm(dif~SV,data=ratio)
-new<-data.frame(SV=c(Boot.low,Boot.up))
+new<-data.frame(SV=Boot.SV.mean)
 temp.predict<-predict.lm(temp.lm,new,interval="confidence")
 #obtain possible difference of the temperature
-temp.dif<-c(temp.predict[1,2],temp.predict[2,3])#it's the lowest dif and highest dif
+temp.dif<-c(temp.predict[,2],temp.predict[,3])#it's the lowest dif and highest dif
 
 
 #Now we can use the MCF calculator to see the change of MCF
