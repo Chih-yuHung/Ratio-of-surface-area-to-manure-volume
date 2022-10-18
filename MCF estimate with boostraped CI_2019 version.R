fe4dcf12bdea188d1421200fa3a11dfdd7a30712
Hung <chih-yu.hung@agr.gc.ca>
2022-10-18 17:33:52 -0400

    update temperature data

--- MCF estimate with boostraped CI_2019 version.R
+++ MCF estimate with boostraped CI_2019 version.R
@@ -10,7 +10,6 @@ library(parallel)
 source("MCF estimator.R",echo = F,local=TRUE) #the MCF calculator function.
 
 ratio<-read.csv("Manure Linear Regression.csv")
-ratio<-ratio[c(-6,-9),] #9 is from Sweden
 #do the bootstrap
 set.seed(2022)
 SV.n<-30 #number of samples we want to boostrap everytime
@@ -22,20 +21,18 @@ Boot.SV<-matrix(sample(ratio$SV,size=B*SV.n,
 Boot.SV.mean<-colMeans(Boot.SV)
  
 #Obtain the percentile
-# Boot.low<-quantile(Boot.SV.mean,prob=0.025,names=FALSE) #0.3903
-# Boot.up<-quantile(Boot.SV.mean,prob=0.975,names=FALSE) #0.4173
-# Boot.low<-quantile(Boot.SV.mean,prob=0.01,names=FALSE) #0.388
-# Boot.up<-quantile(Boot.SV.mean,prob=0.99,names=FALSE) #0.419
+#Boot.low<-quantile(Boot.SV.mean,prob=0.025,names=FALSE) #0.443
+#Boot.up<-quantile(Boot.SV.mean,prob=0.975,names=FALSE) #0.513
 
 #We're going to apply this to the linear regression 
 #and obtain 95% CI for the temperature difference
-temp.lm<-lm(dif~SV,data=ratio)#y = 42.3x-18.4
+temp.lm<-lm(dif~SV,data=ratio)#y = 8.35x-3.92
 new<-data.frame(SV=Boot.SV.mean)
 #The possible differecne of the 10000 bootstrap
 temp.predict<-predict.lm(temp.lm,new,interval="confidence")
 #obtain possible difference of the temperature
-low<-quantile(temp.predict[,2],prob=0.025,names=FALSE) #-3.398
-up<-quantile(temp.predict[,3],prob=0.975,names=FALSE) #0.720
+low<-quantile(temp.predict[,2],prob=0.025,names=FALSE) #-1.142
+up<-quantile(temp.predict[,3],prob=0.975,names=FALSE) #1.290
 temp.dif<-c(low,up)#it's the lowest dif and highest dif
 
 #Now we can use the MCF calculator to see the change of MCF
@@ -52,35 +49,37 @@ max.month<-apply(T.avg,2,which.max)
 #Because 10000 times bootstrap is too large, I split to 5 sets of 2000 
 T.avg.dif<-rep(T.2019,2000)
 #Apply the temperature difference to the max three months
-#manually change paraters, m = 3403*1, 3403*2000, 3403*4000, 3403*6000, 3403*8000
-#the i from 1:2000, 2001:4000, 4001:6000, 6001:8000, 8001:1000
-m<-3403*1
-for (i in 1:2000) {
+#manually change parameters, m = 0, 3403*2000, 3403*4000, 3403*6000, 3403*8000
+#the i from 1:2000, 2001:4000, 4001:6000, 6001:8000, 8001:10000
+m<-3403*8000
+for (i in 8001:10000) {
 for(j in 1:length(T.2019)) {
 n<-3403*(i-1)+j
 T.avg.dif[[n-m]][c(max.month[j]-1,max.month[j],max.month[j]+1)]<-
   T.2019[c(max.month[j]-1,max.month[j],max.month[j]+1),j]+temp.predict[i,1]
-print(round((n-m)/(3403*2000)*100,3)) #show runing progress
+print(round((n-m)/(3403*2000)*100,3)) #show running progress
 }
 }
 
-save(T.avg.dif,file = "T.avg.diff-1.rda") # to save data. need to know that 
+save(T.avg.dif,file = "T.avg.diff-5.rda") # to save data. need to know that 
 #the data name will be T.avg.dif when you load this file
 
 #Calculate the MCF for the input locations
 #under the two removal scenario, early spring-early fall
 #removal scenario was set on May and October
 #Need to change the "1" to 2,3,4,5 for all results.
-load(file = "T.avg.diff-1.rda")
+for (i in 1:5){
+load(file = paste("T.avg.diff-",i,".rda",sep=""))
 cl <- makeCluster(8)
 MCF.2019.p<-c()
 time.s<-Sys.time()
 MCF.2019.p<-parLapply(cl,T.avg.dif,MCF.est)
 print(Sys.time()-time.s)#17 mins with 6806000 
 stopCluster(cl)
+save(MCF.2019.p,file = paste("MCF2019-",i,".rda",sep=""))
+}
+#summary(MCF.2019) #min:0.101, mean:0.189, max 0.339
 
-summary(MCF.2019) #min:0.101, mean:0.189, max 0.339
-save(MCF.2019.p,file = "MCF2019-1.rda")
 
 
 #I save all data to rda data and draw my figure 4B
--- ratio_linear regression.R
+++ ratio_linear regression.R
@@ -3,26 +3,27 @@ library(ggplot2)
 library(ggpubr)
 
 Temp<-read.csv("C:/Users/hungc/OneDrive - AGR-AGR/AAFC/Project 7_surface area ratio/2_mehtods/R/Ratio-of-surface-area-to-manure-volume/Manure Linear Regression.csv")
-#remove the 6 and 9, the 6 is an outlier 
 #I received information that 9 removed manure just before summer,
 #the removal significantly decreased the manure volume and influence the result
 #CY, March 27, 2022
-Temp<-Temp[c(-6,-9),]
+#Temp<-Temp[c(-6,-9),]
+#I removed the data in the csv file, Oct 5, 2022. 
+#Latest data has 3 set data from Fraser farm from 2017-2019
 
 #Linear regression and plot
-#Temp.lm<-lm(dif~SV,data=Temp)
-# a<-round(Temp.lm$coefficients[1],2)       #obtain the coeffiecients
-# b<-round(Temp.lm$coefficients[2],1)
-# R2<-round(summary(Temp.lm)$r.squared,3)   #0.631
-# p.value<-round(summary(Temp.lm)$coefficients[2,4],3) #0.033
+Temp.lm<-lm(dif~SV,data=Temp)
+a<-round(Temp.lm$coefficients[1],2)       #obtain the coeffiecients,-3.92
+b<-round(Temp.lm$coefficients[2],1)       #8.4
+R2<-round(summary(Temp.lm)$r.squared,3)   #0.303
+p.value<-round(summary(Temp.lm)$coefficients[2,4],3) #0.064
 Temp.cor<-cor.test(Temp$SV,Temp$dif)
-R<-round(Temp.cor$estimate,3) #0.795
-p.value<-round(Temp.cor$p.value,3) #0.033
+R<-round(Temp.cor$estimate,3) #0.55
+p.value<-round(Temp.cor$p.value,3) #0.064
 
 #Obtain the bootstrapped results
 # I run this before "MCF estimate with bootstraped CI.R" to obtain bootstrap result
-new<-data.frame(SV=seq(0.33,0.49,length.out=10000))
-temp.predict<-predict.lm(temp.lm,new,interval="confidence")
+new<-data.frame(SV=seq(0.33,0.68,length.out=10000))
+temp.predict<-predict.lm(Temp.lm,new,interval="confidence")
 
 #scatter plot and linear model
 #Output 800 x 600
@@ -55,7 +56,7 @@ plot(0,
      xlab=expression(paste("Surface area / manure volume (",m^-1,")")),
      ylab=expression(paste("T"["diff"]~"(",degree,"C)")),
      las=1,xaxs="i",yaxs="i",
-     xlim=c(0.32,0.50),
+     xlim=c(0.32,0.70),
      ylim=c(-5.0,4),
      cex.lab=1.3)
 polygon(c(rev(new[,1]), new[,1]), c(rev(temp.predict[ ,3]), temp.predict[ ,2]), col = 'grey90', border = NA)
@@ -64,11 +65,13 @@ lines(new[,1],temp.predict[,3],lty = 'dashed', col = 'red')
 points(Temp$SV[Temp$type=="Tank"],Temp$dif[Temp$type=="Tank"],pch=17,cex=1.3)
 points(Temp$SV[Temp$type=="Earthen storage"],Temp$dif[Temp$type=="Earthen storage"],pch=16,cex=1.3)
 abline(lm(Temp$dif~Temp$SV))
-text(0.43,-0.8,expression(paste("r = 0.795, ",italic(P),"< 0.033"))
+text(0.50,-3,expression(paste("r = 0.55, ",italic(P)," = 0.06"))
                        ,pos=4,cex=1.2)
 text(0.32,3.6,"(b)",pos=4,cex=1.3)
-legend(0.33,3,pch=c(17,16),c("Tank","Earthen Storage"),
+legend(0.50,-3,pch=c(17,16),c("Tank","Earthen Storage"),
        bty="n",cex=1.3)
-text(Temp$SV+0.005,Temp$dif+0.02,labels = 1:7,cex=1.3)                          
+legend(0.497,-3.9,fill="grey90","95% confidence interval",
+       bty="n",cex=1.3)
+text(Temp$SV+0.007,Temp$dif+0.02,labels = Temp$TableID,cex=1.3)                          
                            
                            
